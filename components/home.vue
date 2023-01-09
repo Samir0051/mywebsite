c941d8bd4f64a33da4533656064f59cb1e04b4e4
Samir Sanchez Tejada <110410051+Samir0051@users.noreply.github.com>
2023-01-09 12:03:24 -0500

    adds more things to the website

--- components/home.vue
+++ components/home.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <h1>Hey y'all!</h1>
-		<p> Follow me on Instagram </p>
-		<a href="https://www.instagram.com/imdanielleo/" target="_blank">
-			here's the link </a>
+		<p> Connect With Me On LinkedIn! </p>
+		<a href="https://www.linkedin.com/in/samir-sanchez-3ac/" target="_blank">
+			LinkedIn </a>
+            <iframe src="Resume_Samir_SanchezTejada.pdf" width="50%" height="500px">
+            </iframe>
 	</div>
 </template>
